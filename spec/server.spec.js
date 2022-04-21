da86b228c8c1b8fb8c2f1f4d436151c99073f55d
yechan <rhkr4080@naver.com>
2022-04-21 17:57:09 +0900

    [TDD, FxJS 토이프로젝트] -중복확인, 회원가입 TDD 작성 중

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -1,5 +1,21 @@
+import app from '../';
 import request from 'supertest';
 import should from 'should';
-import app from '../';
 
-describe('', () => {});
+describe('User', () => {
+  describe('중복확인', () => {
+    it('email이 중복되지 않는다면 200을 응답한다.', (done) => {});
+
+    it('email이 중복된다면 409를 응답한다.', (done) => {});
+
+    it('nickname이 중복되지 않는다면 200을 응답한다.', (done) => {});
+
+    it('nickname이 중복된다면 409를 응답한다.', (done) => {});
+  });
+
+  describe('회원가입', () => {
+    it('회원가입 성공시 200을 응답한다.', (done) => {});
+
+    it('회원가입에 필요한 email, nickname, password 중 하나라도 없다면 400을 응답한다.', (done) => {});
+  });
+});
